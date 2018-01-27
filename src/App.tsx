c5d2a962d4194100d722e92c0933b7102ba9dd43
zhoutk <zhoutk@189.cn>
2018-01-27 14:01:34 +0800

    antd less.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,15 @@
 import * as React from 'react';
+import { Button } from 'antd';
 import './App.css';
 
-const logo = require('./logo.svg');
-
 class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Button type="primary">Button</Button>
+            </div>
+        );
+    }
 }
 
 export default App;
